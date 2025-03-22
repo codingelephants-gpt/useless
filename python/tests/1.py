777c77384d8b8d0e0ca50ebbf937ae77bf48d0f1
codingelephants <florismooij2014@gmail.com>
2025-03-22 20:01:51 +0100

    fix wrong name in test

--- python/tests/1.py
+++ python/tests/1.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Useless')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'useless')))
 import testinit as useless
 
 useless.output.emptylines(5)
